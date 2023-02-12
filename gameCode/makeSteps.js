c85e18debc0d96f22afc63d6b3dbdf8c6d195ba5
unknown <amberhaf@hotmail.com>
2023-02-12 10:48:28 +0000

    live refresh for bottom slider

--- gameCode/makeSteps.js
+++ gameCode/makeSteps.js
@@ -1,7 +1,7 @@
 function startStep1(){
+  document.getElementById("start").style.display = "none";
   document.getElementById("addNewSlider").style.display = "inline-block";
   document.getElementById("refresh").style.display = "inline-block";
-  document.getElementById("start").style.display = "none";
     if(xyValues.length==4)
     {
       alert("Fill in a new lower and higher guess in step1.js file")
@@ -35,6 +35,19 @@ function startStep1(){
     {
       alert("Try to set the actual amount in step3.js file")
       setTimeout(() => {processStep3()}, 1000*5);
+    }
+    else
+    {
+		  setTimeout(() => {processStep4()}, 1000);
+    }  
+  }
+
+  function processStep4()
+  {
+    if(getComputedStyle(document.getElementById("players")).fontSize === "8px")
+    {
+      alert("Make font size of players bigger in step4.css file")
+      setTimeout(() => {processStep4()}, 1000*5);
     }
     else
     {
